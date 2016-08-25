393daaa77a24449db739c03c79b216a147cb351e
Volker Kettenbach <volker@kettenbach-it.de>
2016-08-25 12:46:46 +0200

    Fixed typo

--- tplink_hs110_cmd.pl
+++ tplink_hs110_cmd.pl
@@ -8,7 +8,7 @@
 # The program is focused on querying most of the data the HS100/110 does provide.
 # Tough it can only turn it on and off and enable/disabled the nightmode. 
 # You can't set things like schedule, wifi network etc.
-# Use the (not so bad at all) samrtphone app for this.
+# Use the (not so bad at all) smartphone app for this.
 # If you want to implement more commands, see tplink-smarthome-commands.txt for a full list
 # and submit your changes as a pull request to my github repository.
 
